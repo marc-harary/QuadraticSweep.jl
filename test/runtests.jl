c6bd55ce5ceeab7eddbd026560f60a7915ec9f01
Marc Harary <marcharary@marcs-mbp.dfci.harvard.edu>
2024-10-21 13:53:25 -0400

    Cleaned up unit tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,1 +1,2 @@
+include("test_basic.jl")
 include("test_bruteforce.jl")
--- test/test_bruteforce.jl
+++ test/test_bruteforce.jl
@@ -5,18 +5,11 @@ using QuadraticSweep
 using Test
 
 # Experiment parameters
-init_seed = 123
-seed_rng = MersenneTwister(init_seed)
+seed_rng = MersenneTwister(123)
 n_iter = 1_000
 k = 10
 n = 20
-lift = (x, y) -> hcat(x, y, x .^ 2, y .^ 2, x .* y)
-score = (x, y) -> cor(x, y)^2
-function score_struct(d)
-    num = (d.s_xy - 1 / d.n * d.s_x * d.s_y)^2
-    den = (d.s_xx - 1 / d.n * d.s_x^2) * (d.s_yy - 1 / d.n * d.s_y^2)
-    return num / den
-end
+score = :r2
 
 # Wrap the test loop inside a testset
 @testset "Quadratic Sweep vs Brute Force" begin
@@ -27,14 +20,13 @@ end
         x, y = rand(data_rng, n), rand(data_rng, n)
 
         # Separate programmatically
-        # idxs_prd = sweep(x, y; k = k, L = lift, S = score_struct, rho = false)
-        idxs_prd = sweep(x, y; k = k, score = :r2)
+        idxs_prd, score_prd = sweep(x, y; k = k, score = score)
 
         # Separate via brute-force
-        idxs_grd, _ = brute_force(x, y, k, score)
-        r2 = cor(x[idxs_grd], y[idxs_grd])^2
+        idxs_grd, score_grd = brute_force(x, y; k = k, score = score)
 
         # Check for match, log seed if not
-        @test sort(idxs_prd) == sort(idxs_grd)
+        @test idxs_prd == idxs_grd
+        @test isapprox(score_prd, score_grd)
     end
 end
