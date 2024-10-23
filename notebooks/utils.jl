d4007a45d718bac5db1060829215b692d345869e
Marc <marcharary98@gmail.com>
2024-10-23 15:15:31 -0400

    Updated utils

--- notebooks/utils.jl
+++ notebooks/utils.jl
@@ -47,17 +47,3 @@ function lp_intersect(A::Matrix{Float64}, B::Matrix{Float64}, tol::Float64 = 1e-
         return model
     end
 end
-
-# Convert a bitmask to a Boolean array
-function bitmask2array(bitmask::Int, num_steps::Int)
-    # Initialize an array to store the binary representation
-    binary_array = Vector{Bool}(undef, num_steps)
-
-    # Extract bits from the bitmask
-    for i in 1:num_steps
-        # Extract the i-th bit from the right
-        binary_array[num_steps - i + 1] = Bool((bitmask >> (i - 1)) & 1)
-    end
-
-    return binary_array
-end
