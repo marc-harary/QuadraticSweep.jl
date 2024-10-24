b0c6c0b8367d23a3b35af953d255df80b8d60959
Marc <marcharary98@gmail.com>
2024-10-24 16:16:19 -0400

    Added functional test cases

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,11 +1,14 @@
 using Test
+using Random
 using QuadraticSweep
 
+include("test_cases.jl")
+
 # Unit tests for error handling and basic functionality
-@testset "Sweep and Brute Force Tests" begin
+@testset "Quadratic sweep tests" begin
 
     # Test input length mismatch error
-    @testset "check_input Tests" begin
+    @testset "Error-handling" begin
         x = [1.0, 2.0, 3.0]
         y = [4.0, 5.0]
         k = 2
@@ -13,33 +16,37 @@ using QuadraticSweep
         # x and y length mismatch
         @test_throws AssertionError sweep(x, y; k = k, score = :r2)
 
-        # Invalid k (k >= n)
+        # Invalid k (k > n)
         y = [4.0, 5.0, 6.0]
-        @test_throws AssertionError sweep(x, y; k = 4, score = :r2)
+        @test_throws AssertionError sweep(x, y; k = 4, score = :dv)
 
         # Invalid score symbol
         @test_throws ErrorException sweep(x, y; k = 2, score = :invalid_symbol)
     end
 
-    # Test brute force algorithm
-    @testset "brute_force Tests" begin
-        x = [1.0, 2.0, 3.0]
-        y = [4.0, 5.0, 6.0]
-        k = 2
+    @testset "Sweep correctness" begin
+        for (score, n, k, seed, idxs_grd, val_grd) in test_cases
+            # Generate data from random seed
+            data_rng = MersenneTwister(seed)
+            x, y = rand(data_rng, n), rand(data_rng, n)
 
-        # Simple brute force test
-        idxs, score = brute_force(x, y; k, score = :tv)
-        @test length(idxs) == k
+            # Run algorithm and check indices and score
+            idxs_sweep, val_sweep = sweep(x, y; k = k, score = score)
+            @test idxs_grd == idxs_sweep
+            @test isapprox(val_grd, val_sweep)
+        end
     end
 
-    # Test sweep algorithm
-    @testset "sweep Tests" begin
-        x = [1.0, 2.0, 3.0, 4.0]
-        y = [2.0, 3.0, 4.0, 5.0]
-        k = 3
+    @testset "Brute-force correctness" begin
+        for (score, n, k, seed, idxs_grd, val_grd) in test_cases
+            # Generate data from random seed
+            data_rng = MersenneTwister(seed)
+            x, y = rand(data_rng, n), rand(data_rng, n)
 
-        # Test sweep for valid inputs
-        idxs, score = sweep(x, y; k, score = :dv)
-        @test length(idxs) == k
+            # Run algorithm and check indices and score 
+            idxs_brute, val_brute = brute_force(x, y; k = k, score = score)
+            @test idxs_grd == idxs_brute
+            @test isapprox(val_grd, val_brute)
+        end
     end
 end
